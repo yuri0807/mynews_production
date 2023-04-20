6c952f74f064ef527a0feb66674a279a35dcc068
Yuri <yurio.1011@gmail.com>
2023-04-20 13:19:28 +0000

    web.phpの修正

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get(‘XXX’, 'AAAcontroller@bbb');
+Route::get('admin/profile/create', 'ProfileController@edit');
