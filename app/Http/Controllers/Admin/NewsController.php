efa7b9f5c0529ed2ead01a95e04413f744d5072e
Yuri <yurio.1011@gmail.com>
2023-04-26 01:50:12 +0000

    README.md を新規に作成

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -43,4 +43,56 @@ class NewsController extends Controller
 
         return redirect('admin/news/create');
     }
+// 以下を追記
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            // 検索されたら検索結果を取得する
+            $posts = News::where('title', $cond_title)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = News::all();
+        }
+        return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+// 以下を追記
+
+    public function edit(Request $request)
+    {
+        // News Modelからデータを取得する
+        $news = News::find($request->id);
+        if (empty($news)) {
+            abort(404);
+        }
+        return view('admin.news.edit', ['news_form' => $news]);
+    }
+
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, News::$rules);
+        // News Modelからデータを取得する
+        $news = News::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $news_form = $request->all();
+        unset($news_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $news->fill($news_form)->save();
+
+        return redirect('admin/news');
+    }
+// 以下を追記
+
+    public function delete(Request $request)
+    {
+        // 該当するNews Modelを取得
+        $news = News::find($request->id);
+
+        // 削除する
+        $news->delete();
+
+        return redirect('admin/news/');
+    }
 }
